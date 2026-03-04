dd489b482eb368d9238180cd4de35513d22cb325
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2026-03-04 11:03:26 +0100

    .

--- examples/books/1.0.0/books/src/main/java/dtu/services/MetricsService.java
+++ examples/books/1.0.0/books/src/main/java/dtu/services/MetricsService.java
@@ -3,8 +3,8 @@ package dtu.services;
 import dtu.services.api.MetricsApi;
 import dtu.services.api.model.Metrics;
 import org.springframework.http.ResponseEntity;
-import dtu.services.library.metrics.MetricsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dtu.services.library.metrics.MetricsAggregator;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RestController
-public class MetricsController implements MetricsApi
+public class MetricsService implements MetricsApi
 {
     @Autowired
-    private MetricsService metrics;
+    private MetricsAggregator metrics;
     private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
 
 
     @Override
-    public ResponseEntity<dtu.services.api.model.Metrics> metrics()
+    public ResponseEntity<Metrics> metrics()
     {
         Metrics stats = mapper.convertValue(metrics.getSnapshot(),Metrics.class);
         return(ResponseEntity.ok(stats));
