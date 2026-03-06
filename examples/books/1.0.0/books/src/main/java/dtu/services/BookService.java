3a1ac2d891bc69101f72317a6f7fba6f8092ac38
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2026-03-06 13:07:04 +0100

    .

--- examples/books/1.0.0/books/src/main/java/dtu/services/BookService.java
+++ examples/books/1.0.0/books/src/main/java/dtu/services/BookService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import dtu.services.api.BooksApi;          // The generated interface
 import dtu.services.api.model.Bog;         // The generated resource model
 import dtu.services.database.BookStore;
+import dtu.services.library.config.OAuth2;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
-import dtu.services.library.config.OAuth2Server;
 import dtu.services.library.config.events.DTUEvents;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +37,7 @@ public class BookService implements BooksApi
     @Override
     public ResponseEntity<List<Bog>> getAll()
     {
-        dtu.services.library.config.OAuth2.authenticate(OAuth2Server.FUSION_HCM);
+        OAuth2.setOutgoingProvider("fusion");
         List<Bog> books = bookstore.getAll();
         return(ResponseEntity.ok(books));
     }
