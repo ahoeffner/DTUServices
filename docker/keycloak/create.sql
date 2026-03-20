f9a93e5457a8e002fc0ff78ea57a53f981058f47
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2026-03-20 10:19:08 +0100

    .

---
+++ docker/keycloak/create.sql
@@ -0,0 +1,2 @@
+create user keycloak with password 'Manager1';
+create database keycloak owner keycloak;
--- docker/oauth/create.sql
+++
@@ -1,2 +0,0 @@
-create user oauth with password 'Manager1';
-create database oauth owner oauth;
